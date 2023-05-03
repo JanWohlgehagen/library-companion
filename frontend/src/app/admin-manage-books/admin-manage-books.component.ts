89edf6dc52781a5d3a7bc0bd1005f054cc4bb194
Tadiaki <n0m1z@hotmail.com>
2023-05-03 15:27:17 +0200

    Started work on admin-manage-books

--- frontend/src/app/admin-manage-books/admin-manage-books.component.ts
+++ frontend/src/app/admin-manage-books/admin-manage-books.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import {MockDataService} from "../../mock_data/mock-data.service";
+import {Book} from "../../Types/types";
+
 
 @Component({
   selector: 'app-admin-manage-books',
   templateUrl: './admin-manage-books.component.html',
   styleUrls: ['./admin-manage-books.component.scss']
 })
-export class AdminManageBooksComponent {
+
+export class AdminManageBooksComponent implements OnInit {
+  control = new FormControl('');
+  filtered: Observable<Book[]> | undefined;
+  books: Book[]
+
+  constructor(private mock: MockDataService) {
+    this.books = this.mock.get_books(10)
+
+  }
+
+  ngOnInit() {
+    this.filtered = this.control.valueChanges.pipe(
+      startWith(null),
+      map(
+        /*const name = typeof value === 'string' ? value : value?.title;*/
+        /*return name ? this._filter(name as string) : this.options.slice();*/
+        (book : string | null) => (book? this._filter(book) : this.books.slice())),);
+  }
+
+
+ /* display(title: Title): string {
+    return title && title.title ? title.title : '';
+  }*/
+
+
+  private _filter(name: string): Book[] {
+    const filterValue = name.toLowerCase();
+    let filteredBooks = this.books.filter(option => {
+      option.title.toLowerCase().includes(filterValue)
+      console.log(filterValue + '= '+ option.title.toLowerCase())
+  })
+    return filteredBooks;
+  }
+
 
 }
+
+
+/*
+export interface Title {
+  title: string;
+}
+*/
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -18,6 +18,10 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {MatMenuModule} from "@angular/material/menu";
 import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -40,7 +44,11 @@ import {MatIcon, MatIconModule} from "@angular/material/icon";
     MatToolbarModule,
     MatButtonModule,
     MatMenuModule,
-    MatIconModule
+    MatIconModule,
+    MatGridListModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
