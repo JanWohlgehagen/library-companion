39e84b89d654cf9bb970fb65e27bf4e2e9439776
JanWohlgehagen <jan.wohlgehagen@gmail.com>
2023-05-16 11:46:19 +0200

    Added tests and IDs in HTML
    The navigation tests are not done.

---
+++ frontend/tests/ui-tests/testcafe-navigation.js
@@ -0,0 +1,58 @@
+import {ClientFunction} from 'testcafe';
+
+fixture('User Registration')
+  .page('localhost:4200');
+
+test('Browse->Login', async test => {
+  const getLocation = ClientFunction(() => document.location.href)
+
+  await test
+    .takeScreenshot()
+    .click('#navBtn-anon')
+    .takeScreenshot()
+    .click('#navBtn-signIn')
+    .takeScreenshot()
+    .expect(getLocation()).contains('login')
+})
+test('Browse->Register', async test => {
+  const getLocation = ClientFunction(() => document.location.href)
+
+  await test
+    .takeScreenshot()
+    .click('#navBtn-anon')
+    .takeScreenshot()
+    .click('#navBtn-register')
+    .takeScreenshot()
+    .expect(getLocation()).contains('register')
+})
+test('Browse->Checkout_CHECKOUT_DENIED_LOGIN_REQUIRED', async test => {
+  const getLocation = ClientFunction(() => document.location.href)
+
+  await test
+    .takeScreenshot()
+    .click('#shoppingCartBtn')
+    .takeScreenshot()
+    .click('#shoppingCartBtn-goToCheckout')
+    .takeScreenshot()
+    .expect(getLocation()).contains('login')
+})
+test('Browse->Checkout', async test => {
+  const getLocation = ClientFunction(() => document.location.href)
+
+  await test
+    .takeScreenshot()
+    .click('#shoppingCartBtn')
+    .takeScreenshot()
+    .click('#shoppingCartBtn-goToCheckout')
+    .takeScreenshot()
+    .typeText('#login-email', 'john@example.com')
+    .typeText('#login-password', 'pw12345')
+    .takeScreenshot()
+    .click('#login-loginBtn')
+    .takeScreenshot()
+    .click('#shoppingCartBtn')
+    .takeScreenshot()
+    .click('#shoppingCartBtn-goToCheckout')
+    .takeScreenshot()
+    .expect(getLocation()).contains('checkout')
+})
