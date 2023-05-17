a2e1f2c4e5ecff3d4248126be32dab32640287cb
JanWohlgehagen <jan.wohlgehagen@gmail.com>
2023-05-17 11:15:23 +0200

    Update testcafe-registration.js

--- frontend/tests/ui-tests/testcafe-registration.js
+++ frontend/tests/ui-tests/testcafe-registration.js
@@ -1,7 +1,7 @@
 import {ClientFunction} from 'testcafe';
 
 fixture('User Registration')
-  .page('localhost:8100/register');
+  .page('http://localhost:8100/register');
 
 test('RegisterIncorrectly_Wrong_Email_Format', async test => {
   const getLocation = ClientFunction(() => document.location.href)
