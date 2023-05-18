b7b7864bfa8ec5d581b11c98c02181cd8fc061d9
JanWohlgehagen <jan.wohlgehagen@gmail.com>
2023-05-18 11:39:05 +0200

    Update about-us.component.ts

--- frontend/src/app/about-us/about-us.component.ts
+++ frontend/src/app/about-us/about-us.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { Loader } from "@googlemaps/js-api-loader"
-import {environment} from "../../environments/environment";
+import {environment} from "../../environments/environment.development";
 
 @Component({
   selector: 'app-about-us',
